c73e83cf98bf70d41e8449eb0b028689121ad196
zhangyuzhi <zhangyuzhi1993@gmail.com>
2021-02-19 23:41:50 +0800

    chore: update devDependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,29 @@
 module.exports = {
-    "extends": ["standard","plugin:jest/recommended"]
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "prettier/prettier": "warn",
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
