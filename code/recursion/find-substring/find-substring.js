ddbab959a194384b763f03a386b1d8b95f34747d
zhangyuzhi <zhangyuzhi1993@gmail.com>
2021-06-10 00:50:34 +0800

    refactor(recursion): use recursion

--- code/recursion/find-substring/find-substring.js
+++ code/recursion/find-substring/find-substring.js
@@ -10,22 +10,42 @@ export function findSubstring(s, words) {
   if (s === null || s.length === 0 || words === null || words.length === 0) {
     return res;
   }
+  const num = words.length;
 
-  return [];
+  // 枚举所有的子串组合
+  const range = (r, arr) => {
+    if (r.length === num) {
+      res.push(r);
+    } else {
+      arr.forEach((item, index) => {
+        let temp = [].concat(arr);
+        temp.splice(index, 1);
+        range(r.concat(item), temp);
+      });
+    }
+  };
+
+  range([], words);
+  return res
+    .map((item) => s.indexOf(item.join('')))
+    .filter((item) => item !== -1)
+    .sort((a, b) => a - b);
 }
 
+// 滑动窗口
 export function findSubstring2Way(s, words) {
   const res = [];
   if (s === null || s.length === 0 || words === null || words.length === 0) {
     return res;
   }
-  const wordsLen = words.length;
-  const singleWordLen = words[0].length;
+  const wordsNum = words.length;
+  const wordLen = words[0].length;
 
-  for (let i = 0; i <= s.length - wordsLen * singleWordLen; i++) {
+  // 窗口的长度 wordsNum * singleWordLen
+  for (let i = 0; i <= s.length - wordsNum * wordLen; i++) {
     const w = [];
-    for (let j = i; j < i + wordsLen * singleWordLen; j = j + singleWordLen) {
-      w.push(s.slice(j, j + singleWordLen));
+    for (let j = i; j < i + wordsNum * wordLen; j = j + wordLen) {
+      w.push(s.slice(j, j + wordLen));
     }
     if (w.sort().join('') === [...words].sort().join('')) {
       res.push(i);
