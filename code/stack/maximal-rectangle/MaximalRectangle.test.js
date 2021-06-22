f735924c74e2ffa524358b063a5abc76b5fba83a
zhangyuzhi <zhangyuzhi1993@gmail.com>
2021-06-22 18:29:15 +0800

    refactor(stack): maximal rectangle

--- code/stack/maximal-rectangle/MaximalRectangle.test.js
+++ code/stack/maximal-rectangle/MaximalRectangle.test.js
@@ -1,4 +1,4 @@
-import { maximalRectangleWay1 } from './MaximalRectangle';
+import { maximalRectangleWay1, maximalRectangleWay2 } from './MaximalRectangle';
 
 test('maximal rectangle', () => {
   expect(
@@ -13,4 +13,19 @@ test('maximal rectangle', () => {
   expect(maximalRectangleWay1([['0']])).toBe(0);
   expect(maximalRectangleWay1([['1']])).toBe(1);
   expect(maximalRectangleWay1([['0', '0']])).toBe(0);
+});
+
+test('maximal rectangle way 2', () => {
+  expect(
+    maximalRectangleWay2([
+      ['1', '0', '1', '0', '0'],
+      ['1', '0', '1', '1', '1'],
+      ['1', '1', '1', '1', '1'],
+      ['1', '0', '0', '1', '0'],
+    ])
+  ).toBe(6);
+  expect(maximalRectangleWay2([])).toBe(0);
+  expect(maximalRectangleWay2([['0']])).toBe(0);
+  expect(maximalRectangleWay2([['1']])).toBe(1);
+  expect(maximalRectangleWay2([['0', '0']])).toBe(0);
 });
